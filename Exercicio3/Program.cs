f7bcc57896046ce304c3a984a0dd905f396ddcc9
Ricardo Carvalho da Costa <115588683+ricardocarvalhodacosta@users.noreply.github.com>
2023-05-09 15:09:05 -0300

    Add files via upload

--- Exercicio3/Program.cs
+++ Exercicio3/Program.cs
@@ -22,7 +22,7 @@ namespace Exercicio3
             aluno.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             aluno.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            Console.WriteLine("NOTA FINAL = " + aluno.NotaFinal().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("NOTA FINAL (Media 60.0) = " + aluno.NotaFinal().ToString("F2", CultureInfo.InvariantCulture));
 
             if (aluno.Resultado() == true)
             {

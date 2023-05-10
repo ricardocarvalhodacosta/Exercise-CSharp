ddf2648feab6165274f30dcab0d89451c68efff1
Ricardo Carvalho da Costa <115588683+ricardocarvalhodacosta@users.noreply.github.com>
2023-05-10 17:51:33 -0300

    Conversor de Dolar para Real
    Exercício feito para calcular o valor do dolar e transformá-lo em reais acrescentando o calculo de 6% do IOF;

---
+++ Exercicio4/ConversorDeMoeda.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio4
+{
+    class ConversorDeMoeda
+    {
+        public double ValorDolar;
+        public double DolarQuantidade;
+
+        public double ComprarDolar ()
+        {
+            double Iof = (DolarQuantidade * 6) / 100;
+            return ValorDolar * (DolarQuantidade + Iof);
+        }
+    }
+}
---
+++ Exercicio4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Exercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConversorDeMoeda conv = new ConversorDeMoeda();
+
+            Console.Write("Qual a cotação do dólar? ");
+            conv.ValorDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dolares você vai comprar? ");
+            conv.DolarQuantidade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Valor a ser pago em reais = " + conv.ComprarDolar().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}

9bbe1acc0ea831a6e3c281588748420995dbcc3f
Ricardo Carvalho da Costa <115588683+ricardocarvalhodacosta@users.noreply.github.com>
2023-05-16 16:06:10 -0300

    Exercicio de Listas
    Exercício para fixação de Listas;

---
+++ Exercicio7/Empregados.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio7
+{
+    class Empregados
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Salario { get; private set; }
+
+        public Empregados(int id, string nome, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+
+        public void aumentoSalario (double porcentual)
+        {
+            Salario +=Salario * porcentual / 100.0;
+        }
+
+        public override string ToString()
+        {
+            return Id + ", " + Nome + ", " + Salario.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}

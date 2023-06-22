3982c3ffbd99199d6cc013b8731075906b85d16a
Ricardo Carvalho da Costa <rickerscosta@gmail.com>
2023-06-22 19:05:20 -0300

    Adicionando exercício 12

---
+++ Exercicio_12/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Globalization;
+using Exercicio_12.Entities;
+
+namespace Exercicio_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            Console.Write("Enter the number of products: ");
+            int x = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= x; i++)
+            {
+                Console.WriteLine($"Product #{i} data:");
+                Console.Write("Common, used or imported (c/u/i)? ");
+                char t = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                switch (t)
+                {
+                    case 'c':
+                        list.Add(new Product(name, price)); 
+                        break;
+
+                    case 'u':
+                        Console.Write("Manufacture date (DD/MM/YYYY): ");
+                        DateTime date = DateTime.Parse(Console.ReadLine());
+                        list.Add(new UsedProduct(name, price, date));
+                        break;
+
+                    case 'i':
+                        Console.Write("Customs Fee: ");
+                        double customsFee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                        list.Add(new ImportedProduct(name, price, customsFee));
+                        break;
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("PRICE TAGS:");
+            foreach (Product product in list)
+            {
+                Console.WriteLine(product.PriceTag());
+            }
+        }
+    }
+}
